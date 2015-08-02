a6ae272bb1390883f92055cf0f8f4076e72d97ec
Chris Dodge <chris@redrat.co.uk>
2015-08-02 13:37:23 +0100

    Added RaceTimingApp project

---
+++ RaceTimingApp/RaceTimingForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RedRat.RaceTiming
+{
+    public partial class RaceTimingForm : Form
+    {
+        public RaceTimingForm()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show( "Hello..." );
+        }
+    }
+}
