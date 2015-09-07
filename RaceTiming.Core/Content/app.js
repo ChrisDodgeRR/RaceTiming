742d5817773888bd0b54b59d41bc80579cc86aa1
AnnaDodge <anna.dodge@live.co.uk>
2015-09-07 17:22:05 +0100

    Edited Table

--- RaceTiming.Core/Content/app.js
+++ RaceTiming.Core/Content/app.js
@@ -3,27 +3,33 @@
   
   app.controller('MainController', function($scope, $http){
      //$http.get("http://www.w3schools.com/angular/customers.php")
-     $http.get("http://0.0.0.0:1234/api/runners")
+     $http.get("/api/runners")
     .success(function(response) {$scope.entrants = response.entrants;});
     
   $scope.ordered_columns = [];
   $scope.all_columns = [{
-    "title": "number",
+    "title": "Number",
+    "ref": "number",
+    "type": "number",
+  }, {
+    "title": "First Name",
+    "ref": "firstName",
     "type": "string",
   }, {
-    "title": "firstName",
+    "title": "Last Name",
+    "ref": "lastName",
     "type": "string",
   }, {
-    "title": "lastName",
-    "type": "string",
-  }, {
-    "title": "dateOfBirth",
+    "title": "DoB",
+    "ref": "dateOfBirth",
     "type": "date",
   }, {
-    "title": "club",
+    "title": "Club",
+    "ref": "club",
     "type": "string",
   }, {
-    "title": "team",
+    "title": "Team",
+    "ref": "team",
     "type": "string",
   }];
 
